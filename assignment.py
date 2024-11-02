9fa83037d118d98c3cd8e88ca9f9f66752095623
upatrickmatthew <upatrick.matthew@gmail.com>
2024-11-02 12:35:18 +0800

    update

---
+++ assignment.py
@@ -0,0 +1,12 @@
+from turtle import *
+import colorsys
+speed(5)
+bgcolor("gray")
+pensize(5)
+for i in range(6):
+    left(60)
+    for i in range(6):
+        forward(100)
+        left(60)
+
+hideturtle()
